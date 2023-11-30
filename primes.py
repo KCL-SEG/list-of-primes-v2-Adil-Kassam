ee18f1ed69bef6efb1408fd7bd47b6adf6daeb6e
Adil Kassam <kassamadil786@gmail.com>
2023-11-30 11:31:42 +0000

    Commit message

--- primes.py
+++ primes.py
@@ -3,4 +3,24 @@
 
 def primes(number_of_primes):
     list = []
-    return list
+    limit = 2
+    if number_of_primes < 1:
+        raise ValueError('Argument was less than 1')
+    else:
+        while len(list) < number_of_primes:
+            
+            
+                    prime  = True
+                    for j in range(2, limit):
+                        if (limit % j == 0):
+                            prime = False
+                            break
+                    if prime:
+                        list.append(limit)
+                    limit += 1
+                
+                    
+
+
+
+        return list
